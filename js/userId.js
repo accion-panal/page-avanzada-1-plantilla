05ca10a63fa41b345c2eb80f393fcc08d2797799
JaackX1 <nicolascabreramillacan@gmail.com>
2023-06-02 14:20:56 -0400

    feat: Post and Send functions added in contact.html

--- js/userId.js
+++ js/userId.js
@@ -1,1 +1,7 @@
 export const UserData = {statusId: 1,companyId: 1};
+
+export const RealtorSendEmailData = {
+    sendEmail: 'nicolascabreramillacan@gmail.com',  /* SendEmail.js */
+    detail: 'nicolascabreramillacan@gmail.com',     /* SendEmailDetail.js */
+    public: 'nicolascabreramillacan@gmail.com',     /* SendEmailPublic.js */
+};
