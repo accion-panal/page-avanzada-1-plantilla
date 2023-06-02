db1b81f7f3549f291539855bca22014793e74c4e
JaackX1 <nicolascabreramillacan@gmail.com>
2023-06-02 18:10:49 -0400

    feat: mail sending functions added to SendEmailPublic with formSubmit and fix getSelects.js

--- js/utils/getSelects.js
+++ js/utils/getSelects.js
@@ -1,31 +1,79 @@
-import	{ getRegiones } from  "../services/PropertiesServices.js";
-import	{ getCommune } from  "../services/PropertiesServices.js";
+import { getRegiones } from "../services/PropertiesServices.js";
+import { getCommune } from "../services/PropertiesServices.js";
 
-const filterSelects = async()=> {
-    let {data} = await getRegiones();
+
+const filterSelects = async () => {
+    let { data } = await getRegiones();
     console.log(data);
-    let region = document.getElementById('region');
 
-    region.innerHTML = data.regions.map((data, i) => {
-        // let regInt =  getRegiones(data.target.value);
-        if(i != 0){
-            return `
-            <option value="${data.id}">${data.name}</option>`;
-        }else{
-            return `
-            <option value="0" selected >Región</option>
-            <option value="${data.id}">${data.name}</option>
+    /* LLENAR FILTROS DE REGION */
+    /* REGION: rescatar value por su id */
+    let region = document.getElementById('region');
+    if (region !== null) {
+        region.innerHTML = data.regions.map((data, i) => {
+            // let regInt =  getRegiones(data.target.value);
+            if (i != 0) {
+                return `
+                <option value="${data.id}">${data.name}</option>
             `;
-            
-        }
-    }).join("");
-   
-        region.addEventListener("change", async (data) => {
-            let aux = await getCommune(data.target.value);
-            document.getElementById("commune").innerHTML = aux.data.map((data) => 
-            `<option value="${data.id}">${data.name}</option>`
-            );
-        });
-    
+            } else {
+                return `
+                <option value="0" selected >Región</option>
+                <option value="${data.id}">${data.name}</option>
+            `;
+
+            }
+        }).join("");
+    }
+    /* REGION: rescatar value por su name */
+    let regionText = document.getElementById('regionText');
+    if (regionText !== null) {
+        regionText.innerHTML = data.regions.map((data, i) => {
+            if (i != 0) {
+                return `<option value="${data.name}">${data.name}</option>`;
+            } else {
+                return `
+                    <option value="0" selected >Región</option>
+                    <option value="${data.name}">${data.name}</option>
+                `;
+            }
+        }).join("");
+    }
+    /* LLENAR FILTROS DE TIPO DE PROPIEDAD */
+    let typeOfProperty = document.getElementById('typeOfProperty');
+    if (typeOfProperty !== null) {
+        typeOfProperty.innerHTML = data.typeOfProperty.map((data, i) => {
+            if (i != 0) {
+                return `<option value="${data.value}">${data.name}</option>`;
+            } else {
+                return `
+                    <option value="0" selected >Tipo Propiedad</option>
+                    <option value="${data.value}">${data.name}</option>
+                `;
+            }
+        }).join("");
+    }
+    /* LLENAR FILTROS DE TIPO DE OPERACION */
+    let operationType = document.getElementById('operationType');
+    if (operationType !== null) {
+        operationType.innerHTML = data.operationType.map((data, i) => {
+            if (i != 0) {
+                return `<option value="${data.value}">${data.name}</option>`;
+            } else {
+                return `
+                    <option value="0" selected >Operacion</option>
+                    <option value="${data.value}">${data.name}</option>
+                `;
+            }
+        }).join("");
+    }
+
+    /*region.addEventListener("change", async (data) => {
+        let aux = await getCommune(data.target.value);
+        document.getElementById("commune").innerHTML = aux.data.map((data) => 
+        `<option value="${data.id}">${data.name}</option>`
+        );
+    }); */
+
 }
 filterSelects();
